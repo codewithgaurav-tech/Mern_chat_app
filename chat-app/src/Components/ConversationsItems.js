cc4cc9f817930ce204146ebc78d8a75381907755
glalwani177 <glalwani177@gmail.com>
2023-08-20 12:41:18 +0530

    fix some ui issues

---
+++ chat-app/src/Components/ConversationsItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./myStyle.css";
+const ConversationsItems = () => {
+  return <div className="">ConversationsItems</div>;
+};
+
+export default ConversationsItems;
