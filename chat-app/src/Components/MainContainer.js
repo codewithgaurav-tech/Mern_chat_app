4b755e6f484177a62ed20382f3865c3b201cedd0
glalwani177 <glalwani177@gmail.com>
2023-08-20 15:54:11 +0530

    created main chat app component

--- chat-app/src/Components/MainContainer.js
+++ chat-app/src/Components/MainContainer.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./myStyle.css"
 import Sidebar from './Sidebar';
 import WorkArea from './WorkArea';
+import ChatArea from './ChatArea';
 
 const MainConatiner = () => {
+    const [conversations, setConversations] = useState([
+        {
+          name: "Gaurav Lalwani",
+          lastMessage: "Message",
+          timeStamp: "today",
+        },
+        {
+          name: "test1 ",
+          lastMessage: "Demo message",
+          timeStamp: "today",
+        },
+      ]);
     return (
         <div className="main-container">
             <Sidebar />
-            <WorkArea />
+            <ChatArea props={conversations[0]}/>
         </div>
     )
 }
